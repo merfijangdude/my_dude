5c6709104e2e9d1b89d198051b1ed78c77581eea
merfi <79623266046@mail.ru>
2024-12-18 18:37:20 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ all/app/main/views.py
@@ -0,0 +1,14 @@
+from flask import render_template, Blueprint
+
+main_page_blueprint = Blueprint("main_page_blueprint", __name__, template_folder="templates")
+
+@main_page_blueprint.route("/", methods= ["GET"])
+def index():
+    print("index")
+    print("down")
+    print("clown")
+    print("mega_down")
+    print("until_state_fullest_down")
+    print("dang it")
+    print("oh dang it")
+    return render_template("user-feed.html")
